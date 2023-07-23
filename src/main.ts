c34c8582c4249f0a90735bb15cfdd4b4cb22c766
andreyka101 <andrejkvasnin236@gmail.com>
2023-07-23 17:14:19 +0300

    кпкп

--- src/main.ts
+++ src/main.ts
@@ -16,8 +16,8 @@ let rabbit = {
 rabbit.__proto__ = animal; // (*)
 
 // теперь мы можем найти оба свойства в rabbit:
-console.log( rabbit.eats ); // true (**)
-console.log( rabbit.jumps ); // true
+console.log( "rabbit.eats = "+rabbit.eats ); // true (**)
+console.log( "rabbit.eats = "+rabbit.jumps ); // true
 
 
 // *LINK - 2 - [[Prototype]] =======================================================
