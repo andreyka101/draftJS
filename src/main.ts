64604f6d285715981eecee93c6ed09c3adaa0ebf
andreyka101 <andrejkvasnin236@gmail.com>
2023-07-26 14:30:27 +0300

    tvtvtv

--- src/main.ts
+++ src/main.ts
@@ -4,22 +4,35 @@ import './style.css'
 // *LINK - 1 - [[Prototype]] =======================================================
 
 
+// let animal = {
+//   eats: true
+// }as any
+// let rabbit = {
+//   jumps: true
+// }as any
+
+
+
+// rabbit.__proto__ = animal; // (*)
+
+// // теперь мы можем найти оба свойства в rabbit:
+// console.log( "rabbit.eats = "+rabbit.eats ); // true (**)
+// console.log( "rabbit.eats = "+rabbit.jumps ); // true
+
+
+// *LINK - 2 - F.prototype =======================================================
+
+
 let animal = {
   eats: true
-}as any
-let rabbit = {
-  jumps: true
-}as any
+};
 
+function Rabbit(name:string ) {
+  return this.name = name;
+}
 
+Rabbit.prototype = animal;
 
-rabbit.__proto__ = animal; // (*)
+let rabbit = new Rabbit("White Rabbit"); //  rabbit.__proto__ == animal
 
-// теперь мы можем найти оба свойства в rabbit:
-console.log( "rabbit.eats = "+rabbit.eats ); // true (**)
-console.log( "rabbit.eats = "+rabbit.jumps ); // true
-
-
-// *LINK - 2 - [[Prototype]] =======================================================
-
-
+alert( rabbit.eats ); // true
