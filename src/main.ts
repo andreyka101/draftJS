6ac0276b1c043995934106ce22968d94c81ad1e2
andreyka101 <andrejkvasnin236@gmail.com>
2023-07-30 20:20:02 +0300

    крр

--- src/main.ts
+++ src/main.ts
@@ -15,29 +15,28 @@ import './style.css'
 
 // rabbit.__proto__ = animal; // (*)
 
-// // теперь мы можем найти оба свойства в rabbit:
+// теперь мы можем найти оба свойства в rabbit:
 // console.log( "rabbit.eats = "+rabbit.eats ); // true (**)
 // console.log( "rabbit.eats = "+rabbit.jumps ); // true
 
 
 // *LINK - 2 - F.prototype =======================================================
 
-//STUB - ошибка хз какая
 
-let animal = {
-  eats: true
-};
+// let animal = {
+//   eats: true
+// };
 
-const Rabbit = (function(this:any,name:string ) {
-  this.name = name;
-}) as any
+// const Rabbit = (function(this:any,name:string ) {
+//   this.name = name;
+// }) as any
 
-Rabbit.prototype = animal;
+// Rabbit.prototype = animal;
 
-let rabbit = new Rabbit("White Rabbit"); //  rabbit.__proto__ == animal
+// let rabbit = new Rabbit("White Rabbit"); //  rabbit.__proto__ == animal
 
-console.log( rabbit.eats ); // true
-console.log( rabbit.name ); // White Rabbit
+// console.log( rabbit.eats ); // true
+// console.log( rabbit.name ); // White Rabbit
 
 // '''''''''''''''''''''''''''''''''''''
 
@@ -45,7 +44,6 @@ console.log( rabbit.name ); // White Rabbit
 //  по умолчанию:
 //  Rabbit.prototype = { constructor: Rabbit }
 
-// let rabbit = new Rabbit(); // наследует от {constructor: Rabbit}
 
 // alert(rabbit.constructor == Rabbit); // true (свойство получено из прототипа)
 
@@ -59,4 +57,49 @@ console.log( rabbit.name ); // White Rabbit
 // let rabbit = new Rabbit("White Rabbit");
 
 // let rabbit2 = new rabbit.constructor("Black Rabbit");
+
+
+// let rabbit = new Rabbit(); // наследует от {constructor: Rabbit}
+
+
+
+// *LINK - 2 - F.prototype =======================================================
+
+
+// ?############# Описание объектов, методов JSDoc ##############
+// https://jsdoc.app/index.html
+
+/**
+ * @method
+ * Тут можем создать описание функции
+ *
+ * @param {string} this - так описываются параметры. Т.к. это метод, опишем только this, который передавать не нужно
+ */
+String.prototype.reverse = function (this: string) {
+  return this.split('').reverse().join('')
+}
+console.log('sffgdfgdfg'.reverse())
+
+/**
+ * Объект с числом и методом
+ */
+
+const myObj = {
+  /**
+   * @argument
+   * Просто число
+   */
+  a:10,
+  /**
+   * @method
+   * Увеличивает a на 1
+   */
+  count() {
+    this.a++
+  }
+}
+
+myObj.count()
+myObj.a
+console.log(myObj)
 
