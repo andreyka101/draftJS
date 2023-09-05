13285b2a506ea0a163cb8a3e82ca4c7427bbb25f
Andreka101 <andrejkvasnin236@gmail.com>
2023-09-05 21:04:51 +0300

    rggrgrgrgr

--- src/main.ts
+++ src/main.ts
@@ -200,14 +200,24 @@ console.log(u.n(4))
 // Если пользователь вводит HTML-теги, они должны обрабатываться как текст.
 
 let LiArrey = [] as any
-let t  ={
-   weet(){
-    console.log("88888")
-   }
-  }
-LiArrey.__proto__ = t
-// console.log(prompt())
-LiArrey.weet
+
+
+// *LINK - Создайте список =======================================================
 
 
 
+
+
+
+
+//=================================================================
+
+// let LiArrey = [] as any
+// let t  ={
+//    weet(){
+//     console.log("88888")
+//    }
+//   }
+// LiArrey.__proto__ = t
+// // console.log(prompt())
+// LiArrey.weet
